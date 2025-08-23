70c82bdc727844d5b5562e6f5842479324380f1f
Pandu Setiawan <panduz33@gmail.com>
2025-08-23 21:45:21 +0800

    change origin

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 const JWT_SECRET = 'your-secret-key-here';
 
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'https://laksana-dummy.vercel.app',
   credentials: true
 }));
 app.use(express.json());
